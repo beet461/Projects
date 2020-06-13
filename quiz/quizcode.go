e1c0edd145d74d486a4f8bb46899defe8e26d395
beet461 <rushvithanthagiri@gmail.com>
2020-06-13 16:24:19 +0100

    Up to Date

--- quiz/quizcode.go
+++ quiz/quizcode.go
@@ -5,13 +5,15 @@ import (
 	"os"
 )
 
-func quiz() {
+func main() {
 	var quest1 string
 	var quest2 string
 	var quest3 string
+	var quest4 string
+	var challenge string
 	var goo string
 	cor := "Correct!"
-	fmt.Println("Welcome to the Grand Quiz, which makes no sense at all!")
+	fmt.Println("Welcome to the 5 question Grand Quiz, which makes no sense at all!")
 	fmt.Println("Rules:", "1. DON'T get it wrong.", "2. Type one character only.", "3. Follow the rules.")
 	fmt.Println("Type in -Quiz.- to start the quiz")
 	fmt.Scan(&goo)
@@ -42,6 +44,28 @@ func quiz() {
 		os.Exit(404)
 	} else {
 		fmt.Println(cor)
+	}
+	fmt.Println("Q4) Is this the last question on this quiz (read the first line of this quiz)?", "Is it: ", "\n", "A) oN", "\n", "B) No", "\n", "C) No")
+	fmt.Scan(&quest4)
+	if quest4 != "Yes" {
+		fmt.Println("Noob")
+		os.Exit(404)
+	} else {
+		fmt.Println(cor)
+	}
+
+	fmt.Println("If you are reading this then you have encountered a challenge question!", "\n", "Challenge) What is the answer to to a question?")
+	fmt.Println("Is it: ", "\n", "A) 101001010011111001010", "\n", "B) - .... . .-. . / .- .-. . / - .-- --- / - --- ... / .. -. / - .... . / --.- ..- . ... - .. --- -..", "\n", "C) 37456048bvh6s76gwvBVT7IGhg4787yg4")
+	fmt.Scan(&challenge)
+	if challenge == "answer" {
+		fmt.Println("Congratulations, you have a certificate of stupid!")
+		os.Exit(398596582049)
+	} else if challenge == "........" {
+		fmt.Println("Woohoo you have got all of them correct!", "\n", "Now go away nerd.")
+		os.Exit(69420)
+	} else {
+		fmt.Println("Still wrong.")
+		os.Exit(404)
 	}
 }
 
