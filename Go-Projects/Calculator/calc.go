5fa7d56777023e1ccbc0e9fee1da5fb33cc56fe5
beet461 <rushvithanthagiri@gmail.com>
2020-07-03 19:22:07 +0100

    Up to Date

--- Go-Projects/Calculator/calc.go
+++ Go-Projects/Calculator/calc.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func start(function string, onum float64, senum float64) float64 {
 	switch function {
@@ -14,6 +17,8 @@ func start(function string, onum float64, senum float64) float64 {
 		fmt.Println("Your answer is", onum/senum)
 	case "%":
 		fmt.Println("Your answer is", (onum/100)*senum)
+	default:
+		os.Exit(707)
 	}
 	var oops float64
 	oops = 404
@@ -24,12 +29,12 @@ func main() {
 	var function string
 	var onum float64
 	var senum float64
-	fmt.Println("Welcome to the calculator!")
-	fmt.Println("Choose a function, +, -, *,/ or % (% is going to be the first number that you typed)")
+	fmt.Println("Choose a function, +, -, *,/, % (% is going to be the first number that you typed) or anything else to exit.")
 	fmt.Scan(&function)
 	fmt.Println("Choose the first number")
 	fmt.Scan(&onum)
 	fmt.Println("Choose the second number")
 	fmt.Scan(&senum)
 	start(function, onum, senum)
+	main()
 }
