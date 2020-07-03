86beed167a8000cdc058cbbde0b52610c7294b7d
beet461 <rushvithanthagiri@gmail.com>
2020-07-03 19:19:40 +0100

    Up to Date

--- Go-Projects/Calculator/calc.go
+++ Go-Projects/Calculator/calc.go
@@ -2,64 +2,34 @@ package main
 
 import "fmt"
 
-func add(a, b float64) float64 {
-	return a +b
-}
-
-func sub(a, b float64) float64 {
-	return a - b
-}
-
-func times(a, b float64) float64 {
-	return a * b
-}
-
-func divi(a, b float64) float64 {
-	return a / b
+func start(function string, onum float64, senum float64) float64 {
+	switch function {
+	case "+":
+		fmt.Println("Your answer is", onum+senum)
+	case "-":
+		fmt.Println("Your answer is", onum-senum)
+	case "*":
+		fmt.Println("Your answer is", onum*senum)
+	case "/":
+		fmt.Println("Your answer is", onum/senum)
+	case "%":
+		fmt.Println("Your answer is", (onum/100)*senum)
+	}
+	var oops float64
+	oops = 404
+	return oops
 }
 
 func main() {
-	var choice string
-	var firstnum float64
-	var secondnum float64
-	onenum := "Choose the first number"
-	secnum := "Choose the second number"
+	var function string
+	var onum float64
+	var senum float64
 	fmt.Println("Welcome to the calculator!")
-	fmt.Println("Choose a function, +, -, * or /")
-	fmt.Scan(&choice)
-	if choice != "" {
-		switch choice {
-		case "+":
-			fmt.Println(onenum)
-			fmt.Scan(&firstnum)
-			fmt.Println(secnum)
-			fmt.Scan(&secondnum)
-			j := add(firstnum, secondnum)
-			fmt.Println("Your answer is ", j)
-		case "-":
-			fmt.Println(onenum)
-			fmt.Scan(&firstnum)
-			fmt.Println(secnum)
-			fmt.Scan(&secondnum)
-			j := sub(firstnum, secondnum)
-			fmt.Println("Your answer is ", j)
-		case "*":
-			fmt.Println(onenum)
-			fmt.Scan(&firstnum)
-			fmt.Println(secnum)
-			fmt.Scan(&secondnum)
-			j := times(firstnum, secondnum)
-			fmt.Println("Your answer is ", j)
-		case "/":
-			fmt.Println(onenum)
-			fmt.Scan(&firstnum)
-			fmt.Println(secnum)
-			fmt.Scan(&secondnum)
-			j := divi(firstnum, secondnum)
-			fmt.Println("Your answer is ", j)
-		default:
-			fmt.Println("Not a valid function!")
-		}
-	}
-	main()
+	fmt.Println("Choose a function, +, -, *,/ or % (% is going to be the first number that you typed)")
+	fmt.Scan(&function)
+	fmt.Println("Choose the first number")
+	fmt.Scan(&onum)
+	fmt.Println("Choose the second number")
+	fmt.Scan(&senum)
+	start(function, onum, senum)
 }
