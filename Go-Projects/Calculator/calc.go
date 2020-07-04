cebd6771edad1ff4e2af9df55a920b695778815c
beet461 <rushvithanthagiri@gmail.com>
2020-07-04 14:19:16 +0100

    Up to Date

--- Go-Projects/Calculator/calc.go
+++ Go-Projects/Calculator/calc.go
@@ -5,6 +5,34 @@ import (
 	"os"
 )
 
+func fraction(typ string) float64 {
+	var frac11 float64
+	var frac12 float64
+	var frac21 float64
+	var frac22 float64
+	fmt.Println("Choose the first fraction.")
+	fmt.Scan(&frac11)
+	fmt.Scan(&frac12)
+	fmt.Println("Choose the second fraction.")
+	fmt.Scan(&frac21)
+	fmt.Scan(&frac22)
+	fracans := (frac11 / frac12) / (frac21 / frac22)
+	fracadd := (frac11 / frac12) + (frac21 / frac22)
+	fracsub := (frac11 / frac12) - (frac21 / frac22)
+	fracmul := (frac11 / frac12) * (frac21 / frac22)
+	switch typ {
+	case "-+":
+		fmt.Println(fracadd)
+	case "--":
+		fmt.Println(fracsub)
+	case "-*":
+		fmt.Println(fracmul)
+	case "-/":
+		fmt.Println(fracans)
+	}
+	return 404
+}
+
 func start(function string, onum float64, senum float64) float64 {
 	switch function {
 	case "+":
@@ -36,8 +64,11 @@ func main() {
 	if exit != "go" {
 		os.Exit(707)
 	}
-	fmt.Println("Choose a function, +, -, *, /, % (% is going to be the first number that you typed)")
+	fmt.Println("Choose a function, +, -, *, /, % (% is going to be the first number that you typed) or - (for fraction and put the function you want after it)")
 	fmt.Scan(&function)
+	if function == "-" {
+		fraction(function)
+	}
 	fmt.Println("Choose the first number")
 	fmt.Scan(&onum)
 	fmt.Println("Choose the second number")
