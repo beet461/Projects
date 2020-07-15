daa912f437147ff914d9155a0b9f1b7594497637
beet461 <rushvithanthagiri@gmail.com>
2020-07-15 12:44:08 +0100

    Up to date

--- Go-Projects/randomizer/main.go
+++ Go-Projects/randomizer/main.go
@@ -9,27 +9,9 @@ import (
 
 func randomize(limid int) int {
 	rand.Seed(time.Now().UnixNano())
-	var randomlimit int
-	switch limid {
-	case 10:
-		randomlimit = rand.Intn(10)
-	case 100:
-		randomlimit = rand.Intn(100)
-	case 1000:
-		randomlimit = rand.Intn(1000)
-	case 10000:
-		randomlimit = rand.Intn(10000)
-	case 100000:
-		randomlimit = rand.Intn(100000)
-	case 1000000:
-		randomlimit = rand.Intn(1000000)
-	}
-	return randomlimit
-}
-
-func mainbit(limig int) {
-	j := randomize(limig)
 	var word string
+	var randomlimit int
+	randomlimit = rand.Intn(limid)
 	fmt.Println("Type in anything to begin! Or exit to exit. Or restart to restart")
 	fmt.Scan(&word)
 	if word == "exit" {
@@ -37,14 +19,15 @@ func mainbit(limig int) {
 	} else if word == "restart" {
 		main()
 	}
-	fmt.Println("Your number is", j)
-	mainbit(limig)
+	fmt.Println("Your number is", randomlimit)
+	randomize(limid)
+	return randomlimit
 }
 
 func main() {
 	var limit int
 	fmt.Println("Welcome to the randomizer! This is the place for random numbers.")
-	fmt.Println("Choose your limit! 10, 100, 1000, 10000, 100000 or 1000000")
+	fmt.Println("Choose your limit!")
 	fmt.Scan(&limit)
-	mainbit(limit)
+	randomize(limit)
 }
