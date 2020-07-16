c27d2ffbe3393adae0ddc6c6b263d9c9b19173de
beet461 <rushvithanthagiri@gmail.com>
2020-07-16 21:11:57 +0100

    Up to date

--- Go-Projects/fibbonacci/main.go
+++ Go-Projects/fibbonacci/main.go
@@ -1,21 +1,19 @@
-Live Demo
-
 package main
 
 import "fmt"
 
 func fibonaci(i int) (ret int) {
-   if i == 0 {
-      return 0
-   }
-   if i == 1 {
-      return 1
-   }
-   return fibonaci(i-1) + fibonaci(i-2)
+	if i == 0 {
+		return 0
+	}
+	if i == 1 {
+		return 1
+	}
+	return fibonaci(i-1) + fibonaci(i-2)
 }
 func main() {
-   var i int
-   for i = 0; i < 10; i++ {
-      fmt.Printf("%d ", fibonaci(i))
-   }
+	var i int
+	for i = 0; i < 10; i++ {
+		fmt.Printf("%d ", fibonaci(i))
+	}
 }
