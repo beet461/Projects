aef058b085b3ec28a4c676e1dba714b108af73fb
beet461 <rushvithanthagiri@gmail.com>
2020-07-16 18:05:36 +0100

    Up to date

--- Rest-API/main.go
+++ Rest-API/main.go
@@ -6,8 +6,21 @@ import (
 	"net/http"
 )
 
+type Article struct{
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: returnAllArticles")
+    json.NewEncoder(w).Encode(Articles)
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePae!")
+	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
@@ -17,5 +30,9 @@ func handleRequests() {
 }
 
 func main() {
+	Articles = []Articles{
+		Article{Title: "Titlele", Desc: "Le Describetion", Content: "Articlecle Contentent"}
+		Article{Title: "Titlele 2", Desc: "Le Describetion 2", Content: "Articlecle Contentent 2"}
+	}
 	handleRequests()
 }
