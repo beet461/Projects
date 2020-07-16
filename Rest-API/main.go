5fcaa8b5a071cb020e57112a5ee531988c3fb3f1
beet461 <rushvithanthagiri@gmail.com>
2020-07-16 18:51:17 +0100

    Up to date

--- Rest-API/main.go
+++ Rest-API/main.go
@@ -24,7 +24,6 @@ func AllArticles(w http.ResponseWriter, r *http.Request) {
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
