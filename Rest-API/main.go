2fcff83d4d4535aee0cd6c7f18eb1636e5e9a1bb
beet461 <rushvithanthagiri@gmail.com>
2020-07-16 18:06:58 +0100

    Up to date

--- Rest-API/main.go
+++ Rest-API/main.go
@@ -31,7 +31,7 @@ func handleRequests() {
 }
 
 func main() {
-	Articles = []Articles{
+	Articles = []Article{
 		Article{Title: "Titlele", Desc: "Le Describetion", Content: "Articlecle Contentent"},
 		Article{Title: "Titlele 2", Desc: "Le Describetion 2", Content: "Articlecle Contentent 2"},
 	}
