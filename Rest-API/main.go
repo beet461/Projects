1830fd2408687b86d17d13e32b700bdf8401dca1
beet461 <rushvithanthagiri@gmail.com>
2020-07-16 18:54:40 +0100

    Up to date

--- Rest-API/main.go
+++ Rest-API/main.go
@@ -1,39 +1,31 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
+    "log"
+    "net/http"
+    "encoding/json"
+    "github.com/gorilla/mux"
 )
 
-type Article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-var Articles []Article
-
-func AllArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Article{
-		Article{Title: "Title", Desc: "Description", Content: "Content"},
-	}
-	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-}
-
+… // Existing code from above
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", AllArticles)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    // creates a new instance of a mux router
+    myRouter := mux.NewRouter().StrictSlash(true)
+    // replace http.HandleFunc with myRouter.HandleFunc
+    myRouter.HandleFunc("/", homePage)
+    myRouter.HandleFunc("/all", returnAllArticles)
+    // finally, instead of passing in nil, we want
+    // to pass in our newly created router as the second
+    // argument
+    log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
-
-	handleRequests()
+    fmt.Println("Rest API v2.0 - Mux Routers")
+    Articles = []Article{
+        Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+    }
+    handleRequests()
 }
