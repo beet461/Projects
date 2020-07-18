91b163701794841dcce5a32f10d7aca9e5bb53fb
beet461 <rushvithanthagiri@gmail.com>
2020-07-18 13:16:49 +0100

    Up to date

--- Api/main.go
+++ Api/main.go
@@ -9,8 +9,14 @@ type server struct{}
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "hello world"}`))
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "get called"}`))
+	case "POST":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message: "post called"}`))
+	}
 }
 
 func main() {
