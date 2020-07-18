35bc3bb92ba80d7e214a7dc84f55a3da9b28a62a
beet461 <rushvithanthagiri@gmail.com>
2020-07-18 13:18:41 +0100

    Up to date

--- Api/main.go
+++ Api/main.go
@@ -16,6 +16,15 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"message: "post called"}`))
+	case "PUT":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message: "put called"}`))
+	case "DELETE":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message: "delete called"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message: "not found"}`))
 	}
 }
 
