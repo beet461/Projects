d81ccbd2a268590d576e61f3cddb24ec61fada9e
beet461 <rushvithanthagiri@gmail.com>
2020-07-18 13:30:10 +0100

    Up to date

--- Api/main.go
+++ Api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -38,35 +40,35 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func params(w http.ResponseWriter, r *http.Request) {
-    pathParams := mux.Vars(r)
-    w.Header().Set("Content-Type", "application/json")
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
 
-    userID := -1
-    var err error
-    if val, ok := pathParams["userID"]; ok {
-        userID, err = strconv.Atoi(val)
-        if err != nil {
-            w.WriteHeader(http.StatusInternalServerError)
-            w.Write([]byte(`{"message": "need a number"}`))
-            return
-        }
-    }
+	userID := -1
+	var err error
+	if val, ok := pathParams["userID"]; ok {
+		userID, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message": "need a number"}`))
+			return
+		}
+	}
 
-    commentID := -1
-    if val, ok := pathParams["commentID"]; ok {
-        commentID, err = strconv.Atoi(val)
-        if err != nil {
-            w.WriteHeader(http.StatusInternalServerError)
-            w.Write([]byte(`{"message": "need a number"}`))
-            return
-        }
-    }
+	commentID := -1
+	if val, ok := pathParams["commentID"]; ok {
+		commentID, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message": "need a number"}`))
+			return
+		}
+	}
 
-    query := r.URL.Query()
-    location := query.Get("location")
+	query := r.URL.Query()
+	location := query.Get("location")
 
-    w.Write([]byte(fmt.Sprintf(`{"userID": %d, "commentID": %d, "location": "%s" }`, userID, commentID, location)))
-
+	w.Write([]byte(fmt.Sprintf(`{"userID": %d, "commentID": %d, "location": "%s" }`, userID, commentID, location)))
+}
 
 func main() {
 	r := mux.NewRouter()
