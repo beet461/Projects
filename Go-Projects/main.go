9976c3244bef47925be4722f3ab580efd1f06b83
beet461 <rushvithanthagiri@gmail.com>
2020-07-28 15:32:08 +0100

    Up to Date

--- Go-Projects/main.go
+++ Go-Projects/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+
+func frac(funx string, nu3, nu4 float64) float64{
+	switch funx {
+	case "+":
+		fmt.Println(nu3 + nu4)
+	case "-":
+		fmt.Println(nu3 - nu4)
+	case "*":
+		fmt.Println(nu3 * nu4)
+	case "/":
+		fmt.Println(nu3 / nu4)
+}
+
 func main() {
 	var funx string
 	var nu1 float64
@@ -21,5 +34,12 @@ func main() {
 		fmt.Println(nu1 / nu2)
 	case "%":
 		fmt.Println((nu1 / 100) * nu2)
+	case "//":
+		fmt.Println("Choose your second fraction.")
+		nu3 := nu1 + nu2
+		fmt.Scan(&nu1, &nu2)
+		nu4 := nu1 + nu2
+		fmt.Println("And now choose your function (no, you cant use // or %).")
+		fmt.Scan(&funx)
 	}
 }
