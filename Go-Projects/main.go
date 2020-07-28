bb671997cf1ee6d0ab56244e7c0758ddf72a281f
beet461 <rushvithanthagiri@gmail.com>
2020-07-28 15:14:26 +0100

    Up to Date

--- Go-Projects/main.go
+++ Go-Projects/main.go
@@ -6,11 +6,9 @@ func main() {
 	var funx string
 	var nu1 float64
 	var nu2 float64
-	fmt.Println("Choose your function, +, -, *, /, %, //(fraction) or exit")
+	fmt.Println("Choose your function, +, -, *, /, %, //(fraction) or exit.")
 	fmt.Scan(&funx)
-	fmt.Println("Choose your numbers")
-	fmt.Scan(&nu1)
-	fmt.Print(funx)
-	fmt.Scan(&nu2)
+	fmt.Println("Choose your numbers (space needed).")
+	fmt.Scan(&nu1, &nu2)
 	fmt.Println(nu1, nu2)
 }
