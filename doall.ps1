c856d4e3d2fb68f2278ed33676184b527234eee0
beet461 <rushvithanthagiri@gmail.com>
2020-07-29 11:25:47 +0100

    Up to date

--- doall.ps1
+++ doall.ps1
@@ -1,9 +1,65 @@
-cd ~/doall
+cd .\Go-Projects\Calculator
 
-.\buildall.ps1
+go build main.go
 
-.\updater.ps1
+cd ..
 
-cd ~/Desktop/Projects
+cd .\Go-Projects\counter
+
+go build main.go
+
+cd ..
+
+cd .\Go-Projects\factorial
+
+go build main.go
+
+cd ..
+
+cd .\Go-Projects\fibbonacci
+
+go build main.go
+
+cd ..
+
+cd .\Go-Projects\go
+
+go build main.go
+
+cd ..
+
+cd .\Go-Projects\heistsim
+
+go build main.go
+
+cd ..
+
+cd .\Go-Projects\quiz
+
+go build main.go
+
+cd ..
+
+cd .\Go-Projects\randomizer
+
+go build main.go
+
+cd ..
+
+cd .\Go-Projects\spaceTravel
+
+go build main.go
+
+cd ..
+
+cd .\Go-Projects\time
+
+go build main.go
+
+cd ..
+
+git add . 
+git commit -m "Up to date"
+git push
 
 .\doall.ps1
--- doall.sh
+++ doall.sh
@@ -1,7 +1,67 @@
+cd ./Go-Projects/Calculator
+
+go build main.go
+
+cd ..
+
+cd ./Go-Projects/counter
+
+go build main.go
+
+cd ..
+
+cd ./Go-Projects/factorial
+
+go build main.go
+
+cd ..
+
+cd ./Go-Projects/fibbonacci
+
+go build main.go
+
+cd ..
+
+cd ./Go-Projects/go
+
+go build main.go
+
+cd ..
+
+cd ./Go-Projects/heistsim
+
+go build main.go
+
+cd ..
+
+cd ./Go-Projects/quiz
+
+go build main.go
+
+cd ..
+
+cd ./Go-Projects/randomizer
+
+go build main.go
+
+cd ..
+
+cd ./Go-Projects/spaceTravel
+
+go build main.go
+
+cd ..
+
+cd ./Go-Projects/time
+
+go build main.go
+
+cd ..
+
+cd ..
+
 git add . 
-
-git commit -m "Up to Date"
-
+git commit -m "Up to date"
 git push
 
 ./doall.sh
