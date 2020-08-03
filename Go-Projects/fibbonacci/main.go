16e8bb26b267113d1259ff745a83827a5355a944
beet461 <rushvithanthagiri@gmail.com>
2020-08-03 13:58:19 +0100

    Up to date

--- Go-Projects/fibbonacci/main.go
+++ Go-Projects/fibbonacci/main.go
@@ -2,21 +2,21 @@ package main
 
 import "fmt"
 
-func fibonaci(i int) (ret int) {
+func fibonacci(i int) (ret int) {
 	if i == 0 {
 		return 0
 	}
 	if i == 1 {
 		return 1
 	}
-	return fibonaci(i-1) + fibonaci(i-2)
+	return fibonacci(i-1) + fibonacci(i-2)
 }
 func main() {
 	var i int
 	var in int
-	fmt.Println("Choose how many numbers of the fibonaci sequence you want to know.")
+	fmt.Println("Choose how many numbers of the fibonacci sequence you want to know.")
 	fmt.Scan(&in)
 	for i = 0; i < in; i++ {
-		fmt.Printf("%d ", fibonaci(i))
+		fmt.Printf("%d ", fibonacci(i))
 	}
 }
