7954045bad6713e7735a7010768b9880acd46be8
XPCMasterX <xpcmasterx007@gmail.com>
2020-08-08 18:37:37 +0100

    Up to date

--- Testing/thepowerlynx.com/api.go
+++ Testing/thepowerlynx.com/api.go
@@ -1,13 +1,17 @@
 package com
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func web() {
-	
+
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/website", web)
-	http.ListenAndServe("www.thepowerlynx.com" r)
+	http.ListenAndServe("www.thepowerlynx.com", r)
 }
