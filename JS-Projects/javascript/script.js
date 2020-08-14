50fa4de381547f42a441ba28598cf0797d3fa205
XPCMasterX <xpcmasterx007@gmail.com>
2020-08-14 17:43:33 +0100

    Up to date

--- JS-Projects/javascript/script.js
+++ JS-Projects/javascript/script.js
@@ -27,8 +27,6 @@ function seven() {
     if (h !== undefined) {
     if (chec === true) {
         document.getElementById("7").innerHTML ="7 x " + af * 10 + " = " + time * 10;
-        a = af * 10
-        time = time * 10
     } else {
         document.getElementById("7").innerHTML = sev;
     }      
