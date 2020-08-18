61ded9d6f43f9884e626d11eed3c196ce1a19fa4
XPCMasterX <xpcmasterx007@gmail.com>
2020-08-18 17:57:29 +0100

    Up to date

--- Under development/Astilectron-gui/App/main.go
+++ Under development/Astilectron-gui/App/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/asticode/go-astilectron"
+)
+
+func main() {
+	// Initialize astilectron
+	var a, _ = astilectron.New(log.New(os.Stderr, "", 0), astilectron.Options{
+		AppName:            "Hi",
+		VersionAstilectron: "0.33.0",
+		VersionElectron:    "`6.1.2`",
+	})
+	defer a.Close()
+
+	// Start astilectron
+	a.Start()
+
+	// Blocking
 }
