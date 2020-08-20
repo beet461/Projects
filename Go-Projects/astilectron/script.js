01aa7610ee6bb7bbf94686b6c03ac29a72459a8d
XPCMasterX <xpcmasterx007@gmail.com>
2020-08-20 18:18:43 +0100

    Up to date

--- Go-Projects/astilectron/script.js
+++ Go-Projects/astilectron/script.js
@@ -1,3 +1,5 @@
+let x = document.getElementById("message").value
+
 document.addEventListener('astilectron-ready', function() {
     astilectron.sendMessage(x, function(message) {
         console.log("received " + message)
