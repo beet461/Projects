6347d33d6a87ecc787b7d3e05b517d23a731d2bb
XPCMasterX <xpcmasterx007@gmail.com>
2020-08-20 17:51:35 +0100

    Up to date

--- Go-Projects/astilectron/main.go
+++ Go-Projects/astilectron/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/asticode/go-astikit"
 	"github.com/asticode/go-astilectron"
 )
 
@@ -19,6 +20,14 @@ func main() {
 
 	// Start astilectron
 	a.Start()
+
+	var w, _ = a.NewWindow("http://127.0.0.1:4000", &astilectron.WindowOptions{
+		Center: astikit.BoolPtr(true),
+		Height: astikit.IntPtr(600),
+		Width:  astikit.IntPtr(600),
+	})
+
+	w.Create()
 
 	// Blocking pattern
 	a.Wait()
