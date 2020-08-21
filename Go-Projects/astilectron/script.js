6eeb5a78a91c5a70b894cb7a2b516d38e2e4a114
beet461 <rushvithanthagiri@gmail.com>
2020-08-21 18:44:28 +0100

    Up to date

--- Go-Projects/astilectron/script.js
+++ Go-Projects/astilectron/script.js
@@ -1,5 +1,4 @@
-function sendmsg() {
-    
+function sendmsg() { 
     document.addEventListener('astilectron-ready', function() {
         // This will send a message to GO
         astilectron.sendMessage("hello", function(message) {
