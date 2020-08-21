2b2d18ee4c1d1a95f79444a9b40db6967e10fcf3
beet461 <rushvithanthagiri@gmail.com>
2020-08-21 18:34:25 +0100

    Up to date

--- Go-Projects/astilectron/script.js
+++ Go-Projects/astilectron/script.js
@@ -1,3 +1,16 @@
+// This will wait for the astilectron namespace to be ready
+document.addEventListener('astilectron-ready', function() {
+    // This will listen to messages sent by GO
+    astilectron.onMessage(function(message) {
+        // Process message
+        if (message === "hello") {
+            return "world";
+        }
+    });
+})
+
+
 function sendmsg() {
-    
+
 }
+
