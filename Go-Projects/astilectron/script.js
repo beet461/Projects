a1f80d63ca5a35e341f8e6d291cf669642666492
beet461 <rushvithanthagiri@gmail.com>
2020-08-21 18:27:45 +0100

    Up to date

--- Go-Projects/astilectron/script.js
+++ Go-Projects/astilectron/script.js
@@ -2,8 +2,8 @@
 function sendmsg() {
     let x = document.getElementById("message").value
     document.addEventListener('astilectron-ready', function() {
-        astilectron.sendMessage(x, function(message) {
-            console.log("received " + message)
+        astilectron.sendMessage(x, function(x) {
+            console.log("received " + x)
             if (message === "world")  {
                 console.log(420)
             }
