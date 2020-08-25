6d54b5ac643414f6ad1b01398433ec1d45774fa0
beet461 <rushvithanthagiri@gmail.com>
2020-08-25 17:23:25 +0100

    Up to date

--- Go-Projects/go/main.go
+++ Go-Projects/go/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	out, err := exec.Command("bash", "cd", "..").Output()
+	out, err := exec.Command("bash", "-c", "cd", "..").Output()
 	if err != nil {
 		fmt.Println(err)
 	}
