5ce6fc911fa2937e31ce3bedeef1c010ff2cdd2e
beet461 <rushvithanthagiri@gmail.com>
2020-08-25 16:49:01 +0100

    Up to date

--- JS-Projects/FileMaker/script.js
+++ JS-Projects/FileMaker/script.js
@@ -2,7 +2,7 @@ function test() {
     try {
         fetch("http://localhost:8080/api/v1/webpack")
             .then((data) => {console.log(data)})
-            .then((response) => {console.log(response)})
+            .then((response) => {console.log(response.message)})
     } catch (err) {
         console.log(err)
     }
