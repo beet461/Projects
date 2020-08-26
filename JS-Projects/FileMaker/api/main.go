814cb32476e08a835b397a662f5b81c5f46f614e
beet461 <rushvithanthagiri@gmail.com>
2020-08-26 18:20:43 +0100

    Up to date

--- JS-Projects/FileMaker/api/main.go
+++ JS-Projects/FileMaker/api/main.go
@@ -28,9 +28,9 @@ import (
 */
 
 type Post struct {
-	ID string `json:"id"`
-  Title string `json:"title"`
-  Body string `json:"body"
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
 }
 
 var posts []Post
