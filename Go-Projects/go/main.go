c1dbb75ccc0a055e36558c9c57f215cdfaa750f4
beet461 <rushvithanthagiri@gmail.com>
2020-08-26 17:13:47 +0100

    Up to date

--- Go-Projects/go/main.go
+++ Go-Projects/go/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"os/user"
 )
 
 func main() {
 	out, err := exec.Command("cmd", "/c", "dir").Output()
+	me := user.Current()
 	if err != nil {
 		fmt.Println(err)
 	}
 	output := string(out[:])
 	fmt.Println(output)
+	fmt.Println(me)
 	//fmt.Println(out)
 	//	fmt.Println(err)
 }
