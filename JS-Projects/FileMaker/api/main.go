ae15cd0caddbe5f0ca5c77839ce8548418826922
beet461 <rushvithanthagiri@gmail.com>
2020-08-26 18:26:40 +0100

    Up to date

--- JS-Projects/FileMaker/api/main.go
+++ JS-Projects/FileMaker/api/main.go
@@ -129,9 +129,11 @@ package main
   func main() {
 
 	router := mux.NewRouter()  
-	posts = append(posts, Post{ID: "1", Title: "My first post", Body:      "This is the content of my first post"})  router.HandleFunc("/posts", getPosts).Methods("GET")
+	posts = append(posts, Post{ID: "1", Title: "My first post", Body:      "This is the content of my first post"}) 
+	 router.HandleFunc("/posts", getPosts).Methods("GET")
 	router.HandleFunc("/posts", createPost).Methods("POST")
 	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
 	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
-	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")http.ListenAndServe(":8000", router)
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", router))
   }
