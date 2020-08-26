08de1725cffef9ad9ce889d4bf1e13d31cadc26f
beet461 <rushvithanthagiri@gmail.com>
2020-08-26 18:10:28 +0100

    Up to date

--- JS-Projects/FileMaker/src/main.go
+++ JS-Projects/FileMaker/src/main.go
@@ -16,6 +16,7 @@ func makego(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			fmt.Println(err)
 		}
+		fmt.Println("body = ", body)
 		exec.Command("ls").Output()
 		w.Header().Set("Content-Type", "application/json")
 		enableCors(&w)
