5116a87b8f25b7b702e2b2863d459aa27fad890e
beet461 <rushvithanthagiri@gmail.com>
2020-08-26 18:21:57 +0100

    Up to date

--- JS-Projects/FileMaker/api/main.go
+++ JS-Projects/FileMaker/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
@@ -42,6 +43,7 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 	post.ID = strconv.Itoa(rand.Intn(1000000))
 	posts = append(posts, post)
 	json.NewEncoder(w).Encode(&post)
+	fmt.Println(post)
 }
 
 func enableCors(w *http.ResponseWriter) {
