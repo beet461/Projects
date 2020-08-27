dcc1df902ec84aa7fbc8a4eb2bc37a51c1bf5571
beet461 <rushvithanthagiri@gmail.com>
2020-08-27 19:03:10 +0100

    Up to date

--- Go-Projects/go/main.go
+++ Go-Projects/go/main.go
@@ -1,10 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	var option string
 	fmt.Println("What do you want to do?")
-	fmt.Println("Options : Make a file (make), Delete a file (del), CD somewhere (cd)")
+	fmt.Println("Options : Make a file (make), Delete a file (del), CD somewhere (cd); anything else is to exit.")
 	fmt.Scan(&option)
+	if option == "make" {
+		make()
+	} else if option == "del" {
+		del()
+	} else if option == "cd" {
+		cd()
+	} else {
+		fmt.Println("Do you really want to exit?")
+		fmt.Scan(&option)
+		if option == "no" {
+			main()
+		} else {
+			var one int
+			one = "bye"
+			os.Exit(one)
+		}
+	}
 }
