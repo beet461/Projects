94e8dec4c6eb01a8de8f48df07392e3ccac4dd42
beet461 <rushvithanthagiri@gmail.com>
2020-08-27 18:56:06 +0100

    Up to date

--- Go-Projects/go/main.go
+++ Go-Projects/go/main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"os/exec"
-	"os/user"
-)
-
 func main() {
-	var yesno string
-	path := "~/Desktop/Projects/Go-Projects/go/"
-	exec.Command("cmd", "/c", "cd", path).Output()
-	exec.Command("cmd", "/c", "fsutil", "file", "createnew", "test", "0").Output()
-	me, _ := user.Current()
-	fmt.Println(me)
-	fmt.Println("Would you like to delete file? (yes or no)")
-	fmt.Scan(&yesno)
-	if yesno == "yes" {
-		exec.Command("cmd", "/c", "del", "test").Output()
-	} else {
-		os.Exit(200)
-	}
+
 }
