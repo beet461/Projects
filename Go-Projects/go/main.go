1a68b46484a5eeb065327dce85ba233e4ae1a4f7
beet461 <rushvithanthagiri@gmail.com>
2020-08-27 18:41:45 +0100

    Up to date

--- Go-Projects/go/main.go
+++ Go-Projects/go/main.go
@@ -11,7 +11,7 @@ func main() {
 	var yesno string
 	path := "~/Desktop/Projects/Go-Projects/go/"
 	exec.Command("cmd", "/c", "cd", path).Output()
-	out, err := exec.Command("cmd", "/c", "fsutil", "file", "createnew", "test", "0").Output()
+	exec.Command("cmd", "/c", "fsutil", "file", "createnew", "test", "0").Output()
 	me, _ := user.Current()
 	if err != nil {
 		fmt.Println(err)
