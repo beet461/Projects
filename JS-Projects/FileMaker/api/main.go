7c764d2f9f20ef89b351c2721c1f17a384894348
beet461 <rushvithanthagiri@gmail.com>
2020-08-27 18:19:45 +0100

    Up to date

--- JS-Projects/FileMaker/api/main.go
+++ JS-Projects/FileMaker/api/main.go
@@ -17,7 +17,7 @@ func makego(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Println("body = ", body)
 		strconv := string(body[:])
-		fmt.Printf("strconv = ", strconv)
+		fmt.Println("strconv = ", strconv)
 		//exec.Command("cmd", "/c", "").Output()
 		w.Header().Set("Content-Type", "application/json")
 		enableCors(&w)
