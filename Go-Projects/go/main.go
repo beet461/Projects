8651708f94e72d4f5bde4f5b2b477d73d681d6dd
beet461 <rushvithanthagiri@gmail.com>
2020-08-27 18:59:40 +0100

    Up to date

--- Go-Projects/go/main.go
+++ Go-Projects/go/main.go
@@ -6,5 +6,5 @@ func main() {
 	var option string
 	fmt.Println("What do you want to do?")
 	fmt.Println("Options : Make a file (make), Delete a file (del), CD somewhere (cd)")
-
+	fmt.Scan(&option)
 }
