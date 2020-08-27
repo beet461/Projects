971053a160b36f0adc3097b496ec889e74cec1ab
beet461 <rushvithanthagiri@gmail.com>
2020-08-27 19:08:58 +0100

    Up to date

--- Go-Projects/go/main.go
+++ Go-Projects/go/main.go
@@ -5,29 +5,31 @@ import (
 	"os"
 )
 
-func make() {
+func make(path) {
+	fmt.Println("Please type in your path , e.g. ~/Desktop/...")
+	fmt.Scan(&path)
+}
+
+func del(path) {
 
 }
 
-func del() {
-
-}
-
-func cd() {
+func cd(path) {
 
 }
 
 func main() {
+	var path string
 	var option string
 	fmt.Println("What do you want to do?")
 	fmt.Println("Options : Make a file (make), Delete a file (del), CD somewhere (cd); anything else is to exit.")
 	fmt.Scan(&option)
 	if option == "make" {
-		make()
+		make(path)
 	} else if option == "del" {
-		del()
+		del(path)
 	} else if option == "cd" {
-		cd()
+		cd(path)
 	} else {
 		fmt.Println("Do you really want to exit?")
 		fmt.Scan(&option)
