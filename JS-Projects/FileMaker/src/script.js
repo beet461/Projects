23617fe6da7de1cd85ae4d46926a1198bebbaad2
beet461 <rushvithanthagiri@gmail.com>
2020-08-27 18:13:28 +0100

    Up to date

--- JS-Projects/FileMaker/src/script.js
+++ JS-Projects/FileMaker/src/script.js
@@ -15,7 +15,7 @@ function makego() {
     try {
         fetch("192.168.0.28:8080/api/v1/makego", params)
             .then((data) => {console.log(data)})
-            .then((response) => {console.log("this si the response")})
+            .then((response) => {console.log("this is the response")})
     } catch (err) {
         console.log(err)
     }
