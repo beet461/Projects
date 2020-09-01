bd86baf3f9a14a8c39ed43edad30b211f00de725
beet461 <rushvithanthagiri@gmail.com>
2020-09-01 17:30:00 +0100

    Up to date

--- JS-Projects/FileMaker/src/script.js
+++ JS-Projects/FileMaker/src/script.js
@@ -2,9 +2,8 @@ function register() {
   console.log(document.getElementById("email").value)
   console.log(document.getElementById("usrnm").value)
   console.log(document.getElementById("pswrd").value)
-}
 
-var email = document.getElementById("email").value
+  var email = document.getElementById("email").value
 var usrnm = document.getElementById("usrnm").value
 var pswrd = document.getElementById("pswrd").value
 
@@ -30,3 +29,6 @@ fetch("http://localhost:8080/api/v1/makego", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+
+}
+
