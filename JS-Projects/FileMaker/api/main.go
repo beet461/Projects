8f03c2755cb4e83ac435b0166605a306ef09e0cc
beet461 <rushvithanthagiri@gmail.com>
2020-09-01 16:23:08 +0100

    Up to date

--- JS-Projects/FileMaker/api/main.go
+++ JS-Projects/FileMaker/api/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 
+	key "github.com/beet461/Importable/KeyGen"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 type Data struct {
@@ -16,7 +19,10 @@ type Data struct {
 }
 
 func makego(w http.ResponseWriter, r *http.Request) {
+
 	if r.Method == "POST" {
+		//get json input
+
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			fmt.Println(err)
@@ -29,8 +35,55 @@ func makego(w http.ResponseWriter, r *http.Request) {
 
 		var data Data
 		json.Unmarshal([]byte(body), &data)
-		fmt.Println(data.Username)
+		if data.Username == "nil" {
+			fmt.Println("Username is nil")
+		} else if data.Password == "" {
+			fmt.Println("Password is nil")
+		} else {
+			fmt.Println("Everything is valid")
+		}
+		fmt.Println("Username =", data.Username)
+		fmt.Println("Password =", data.Password)
 
+		//db connect
+
+		username := data.Username
+		randkey := key.Gen()
+
+		const (
+			host     = "localhost"
+			port     = 5432
+			user     = "postgres"
+			password = "shashi3969"
+			dbname   = "FileMaker"
+		)
+
+		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+			host, port, user, password, dbname)
+
+		db, err := sql.Open("postgres", psqlInfo)
+		if err != nil {
+			panic(err)
+		}
+
+		defer db.Close()
+
+		err = db.Ping()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Username =", username, "key = ", randkey)
+
+		sqlStatement := fmt.Sprintf(`
+	INSERT INTO api_keys (username, api_key)
+	VALUES ('%v', '%v');`, data.Username, randkey)
+
+		_, err = db.Exec(sqlStatement)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
