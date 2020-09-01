59a8f9545d400fdb5cc2c3e4d44d98907db84d2b
beet461 <rushvithanthagiri@gmail.com>
2020-09-01 17:30:22 +0100

    Up to date

--- JS-Projects/FileMaker/src/script.js
+++ JS-Projects/FileMaker/src/script.js
@@ -4,31 +4,31 @@ function register() {
   console.log(document.getElementById("pswrd").value)
 
   var email = document.getElementById("email").value
-var usrnm = document.getElementById("usrnm").value
-var pswrd = document.getElementById("pswrd").value
+  var usrnm = document.getElementById("usrnm").value
+  var pswrd = document.getElementById("pswrd").value
 
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-var input = {
-  Email : email,
-  Username : usrnm,
-  Password : pswrd
-}
+  var input = {
+    Email : email,
+    Username : usrnm,
+    Password : pswrd
+  }
 
-var raw = JSON.stringify({input});
+  var raw = JSON.stringify({input});
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
 
-fetch("http://localhost:8080/api/v1/makego", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  fetch("http://localhost:8080/api/v1/makego", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 
 }
 
