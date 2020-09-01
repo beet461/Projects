3acdfb7912d45f9016ca678b57448a2532a873b6
beet461 <rushvithanthagiri@gmail.com>
2020-09-01 17:28:35 +0100

    Up to date

--- JS-Projects/FileMaker/src/script.js
+++ JS-Projects/FileMaker/src/script.js
@@ -4,10 +4,20 @@ function register() {
   console.log(document.getElementById("pswrd").value)
 }
 
+var email = document.getElementById("email").value
+var usrnm = document.getElementById("usrnm").value
+var pswrd = document.getElementById("pswrd").value
+
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
-var raw = JSON.stringify({"username":"heeeeeeellllllllo","password":"heeeeeeelllllllloo"});
+var input = {
+  Email : email,
+  Username : usrnm,
+  Password : pswrd
+}
+
+var raw = JSON.stringify({input});
 
 var requestOptions = {
   method: 'POST',
