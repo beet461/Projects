12d29aab2b62b5fbc2e4292ed3fcf860f37982a3
rajuanthagiri <rajuanthagiri@gmail.com>
2020-11-13 15:17:44 +0000

    hello

--- JS-Projects/Calculator/script.js
+++ JS-Projects/Calculator/script.js
@@ -1,4 +1,4 @@
 function ans() {
-   const x = document.getElementById("name").value;
-   document.getElementById("ans").innerHTML = eval(x);
+    const x = document.getElementById('name').value;
+    document.getElementById('ans').innerHTML = eval(x);
 }
